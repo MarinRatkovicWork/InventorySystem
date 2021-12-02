e96868d5945b7f89845eadada6d3400ed8b42b58
MarinRatkovicWork <86005829+MarinRatkovicWork@users.noreply.github.com>
2021-12-02 14:11:06 +0100

    First Coomite
    Player Walking- done
    Player Animation -done

---
+++ Exordium_ProgrammerAssignment/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 1.4f;
+    public Rigidbody2D Rb;
+    public Animator animator;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.magnitude);
+    }
+
+    void FixedUpdate()
+    {
+        Rb.MovePosition(Rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
